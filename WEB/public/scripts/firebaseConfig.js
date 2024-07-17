d516158c46db3b7b61a63e9ced378377c910ff78
G.Caneca <goncalobcaneca02@outlook.com>
2024-07-17 23:41:55 +0100

    firebase

--- WEB/public/scripts/firebaseConfig.js
+++ WEB/public/scripts/firebaseConfig.js
@@ -1,13 +1,14 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCqj868JLR8BpZKLsFqFk52EPfkX8qTNz8",
-    authDomain: "maos-que-ajudam-gcaneca.firebaseapp.com",
-    databaseURL: "https://maos-que-ajudam-gcaneca-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "maos-que-ajudam-gcaneca",
-    storageBucket: "maos-que-ajudam-gcaneca.appspot.com",
-    messagingSenderId: "925222522786",
-    appId: "1:925222522786:web:04b1579df35531316b6cfb"
+    apiKey: "AIzaSyDeYTwlTtSfJa4_2Y_HNJE-poEsuheLL3M",
+  authDomain: "maos-que-ajudam-sud.firebaseapp.com",
+  databaseURL: "https://maos-que-ajudam-sud-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "maos-que-ajudam-sud",
+  storageBucket: "maos-que-ajudam-sud.appspot.com",
+  messagingSenderId: "24254214401",
+  appId: "1:24254214401:web:f386b6227e752552a36b00",
+  measurementId: "G-DN6EMRZFM6"
 };
 
 const app = initializeApp(firebaseConfig);
