7e20af3ba235b1c721b6b82c5bc8e470e507f47c
G.Caneca <goncalobcaneca02@outlook.com>
2024-07-18 04:18:58 +0100

    funciona

--- WEB/public/scripts/firebaseDatabase.js
+++ WEB/public/scripts/firebaseDatabase.js
@@ -1,22 +1,30 @@
 // import { getDatabase, ref, set } from "firebase/database";
 import { getDatabase, ref, set, child, get } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js';
 import firebaseApp from "./firebaseConfig.js";
-
-const database = getDatabase(firebaseApp);
-
-export const StorePedido = (objData) => {
-    set(ref(database, 'pedidos'), { ...objData });
+import { getFirestore,collection, addDoc, getDocs, setDoc,doc} from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
+const db = getFirestore(firebaseApp);
+export async function StorePedido(objData,docId=1)
+{
+    
+    try {
+if(docId != 1)
+{
+    const docRef = await setDoc(doc(db, "pedidos",docId),objData);
+    
+}
+else
+{
+    const docRef = await addDoc(collection(db, "pedidos"),objData);
+}
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
 }
 
-export const RequestPedidos = () => {
-    const dbRef = ref(database);
-    return get(child(dbRef, 'pedidos')).then((snapshot) => {
-        if (snapshot.exists()) {
-            return snapshot.val();
-        } else {
-            return null
-        }
-    }).catch((error) => {
-        console.error(error);
-    });
+export async function RequestPedidos (userid)
+ {
+    const querySnapshot = await getDocs(collection(db, "pedidos"));
+   
+    return querySnapshot;
+
 }
